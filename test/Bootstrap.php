229d7d6be60b71cc364d71da9dd8dd90a8308ba2
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-06-19 16:26:31 +0700

    done updating dependency and test files

---
+++ test/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// always require front controller so we can load
+// all of the functionality from the framework
+require_once FCPATH . 'index.php';
+
+// This is the bootstrapper starter for PHP Test Unit
